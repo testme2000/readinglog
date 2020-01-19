1c8008ea60dda55937c03dc47f7bc1dc20a8ceb9
Nitin Dixit <nitin.dixit@ncr.com>
2020-01-19 17:20:01 -0600

    Created initial project template files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mainboard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Mainboard from '@/components/mainboard.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Mainboard
   }
 }
 </script>
