582eb6a34352930f8cf836d71fc977d553b015cb
Nitin Dixit <nitin.dixit@ncr.com>
2020-01-19 17:41:00 -0600

    Added two new components

---
+++ src/components/mainboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Booklist />
+    </div>
+</template>
+
+<script>
+    import Booklist from '@/components/booklist.vue'
+
+    export default {
+        name: 'Mainboard',
+        components: {
+            Booklist
+        },
+        data : function() {
+            return {
+                status : true
+            }
+        }        
+    }
+</script>
