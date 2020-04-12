1cf69ec6c8c03ee170367a731980ae3c3ba25905
Nitin Dixit <nitin.dixit@ncr.com>
2020-04-12 16:19:13 -0500

    AddBook form router created

---
+++ src/views/AddBook.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <createbookentry></createbookentry>
+  </div>
+</template>
+
+<script>
+    import createbookentry from '@/components/createbookentry.vue'
+    export default {
+        name: 'addbook',
+        components: {
+            createbookentry
+        }        
+    }
+</script>
